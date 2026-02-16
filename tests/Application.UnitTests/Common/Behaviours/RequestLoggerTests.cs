91f01ad11c918394f403918aa9be30429e588bb1
JonathanArroyaveGonzalez <arroyave3000g@gmail.com>
2026-02-15 22:57:48 -0500

    test: actualizar tests unitarios

--- tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
+++ tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -1,6 +1,6 @@
 ﻿using SAPFIAI.Application.Common.Behaviours;
 using SAPFIAI.Application.Common.Interfaces;
-using SAPFIAI.Application.TodoItems.Commands.CreateTodoItem;
+using SAPFIAI.Application.Users.Commands.Login;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
@@ -9,14 +9,14 @@ namespace SAPFIAI.Application.UnitTests.Common.Behaviours;
 
 public class RequestLoggerTests
 {
-    private Mock<ILogger<CreateTodoItemCommand>> _logger = null!;
+    private Mock<ILogger<LoginCommand>> _logger = null!;
     private Mock<IUser> _user = null!;
     private Mock<IIdentityService> _identityService = null!;
 
     [SetUp]
     public void Setup()
     {
-        _logger = new Mock<ILogger<CreateTodoItemCommand>>();
+        _logger = new Mock<ILogger<LoginCommand>>();
         _user = new Mock<IUser>();
         _identityService = new Mock<IIdentityService>();
     }
@@ -26,9 +26,9 @@ public class RequestLoggerTests
     {
         _user.Setup(x => x.Id).Returns(Guid.NewGuid().ToString());
 
-        var requestLogger = new LoggingBehaviour<CreateTodoItemCommand>(_logger.Object, _user.Object, _identityService.Object);
+        var requestLogger = new LoggingBehaviour<LoginCommand>(_logger.Object, _user.Object, _identityService.Object);
 
-        await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+        await requestLogger.Process(new LoginCommand { Email = "test@test.com", Password = "password" }, new CancellationToken());
 
         _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
     }
@@ -36,9 +36,9 @@ public class RequestLoggerTests
     [Test]
     public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
     {
-        var requestLogger = new LoggingBehaviour<CreateTodoItemCommand>(_logger.Object, _user.Object, _identityService.Object);
+        var requestLogger = new LoggingBehaviour<LoginCommand>(_logger.Object, _user.Object, _identityService.Object);
 
-        await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+        await requestLogger.Process(new LoginCommand { Email = "test@test.com", Password = "password" }, new CancellationToken());
 
         _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Never);
     }
